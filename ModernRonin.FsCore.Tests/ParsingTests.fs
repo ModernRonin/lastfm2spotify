7130b5138b37a23034bc662589fbd45760209d33
Mark Hajek <markus@definedcrowd.com>
2019-01-26 19:17:25 +0000

    * tests for parseInt * reorganized test names for better readability

---
+++ ModernRonin.FsCore.Tests/ParsingTests.fs
@@ -0,0 +1,16 @@
+﻿module ``Parsing``
+
+open NUnit.Framework
+open FsUnit
+open ModernRonin.FsCore.Parsing
+
+module parseInt=
+    [<Test>]
+    let ``returns None if string is not an int``()=
+        parseInt "bla" |> should equal None
+
+    [<Test>]
+    let ``returns Some <number> if the string is an int``()=
+        parseInt "13" |> should equal (Some 13)
+
+
---
+++ ModernRonin.FsCore.Tests/StringsTests.fs
@@ -0,0 +1,25 @@
+﻿module ``Strings``
+
+open NUnit.Framework
+open FsUnit
+open ModernRonin.FsCore.Strings
+
+
+module ``Active pattern Start``=
+    [<Test>]
+    let ``returns None if <whole> does not begin with <part>``()=
+        match "alpha centauri" with
+        | Start "bravo" _ -> false
+        | _ -> true
+        |> should be True
+
+    [<Test>]
+    let ``returns Some with everything after <part> in <whole>``()=
+        match "alpha centauri" with
+        | Start "alpha " x -> Some x
+        | _ -> None
+        |> should equal (Some "centauri")
+    
+
+
+
