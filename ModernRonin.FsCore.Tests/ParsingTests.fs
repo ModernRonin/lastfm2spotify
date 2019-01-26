655cd58e407e3de34fd5ce2a2b9a191bc23f5b5e
Mark Hajek <markus@definedcrowd.com>
2019-01-26 19:35:58 +0000

    completed test coverage for MR.FsCore

--- ModernRonin.FsCore.Tests/ParsingTests.fs
+++ ModernRonin.FsCore.Tests/ParsingTests.fs
@@ -28,3 +28,28 @@ module ``Active pattern Int``=
         | Int n -> n
         | _ -> 0
         |> should equal 13
+
+module ``queryStringToMap``=
+    [<Test>]
+    let ``returns a key-value map of the encoded parameters``()=
+        let result= queryStringToMap "alpha=13&bravo=nix&charlie=..."
+        result |> should haveCount 3
+        result.["alpha"] |> should equal "13"
+        result.["bravo"] |> should equal "nix"
+        result.["charlie"] |> should equal "..."
+
+    [<Test>]
+    let ``deals correctly with empty values``()=
+        let result= queryStringToMap "alpha=13&bravo=&charlie=..."
+        result |> should haveCount 3
+        result.["alpha"] |> should equal "13"
+        result.["bravo"] |> should equal ""
+        result.["charlie"] |> should equal "..."
+
+    [<Test>]
+    let ``filters out incomplete key-value pairs``()=
+        let result= queryStringToMap "alpha=13&bravo&charlie=..."
+        result |> should haveCount 2
+        result.["alpha"] |> should equal "13"
+        result.["charlie"] |> should equal "..."
+        
