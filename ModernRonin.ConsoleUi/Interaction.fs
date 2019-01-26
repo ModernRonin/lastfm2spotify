e5305f1cf79ee58437e49a36c5d4544cdbfe9712
Mark Hajek <markus@definedcrowd.com>
2019-01-26 23:15:20 +0000

    extracted functions for writing/reading/displaying a prompt

--- ModernRonin.ConsoleUi/Interaction.fs
+++ ModernRonin.ConsoleUi/Interaction.fs
@@ -1,51 +1,77 @@
 ﻿namespace ModernRonin.ConsoleUi
 
-module Interaction=
+open System
+open ModernRonin.FsCore.Parsing
 
-    open System
-    open ModernRonin.FsCore.Parsing
-    
-    let setupMaximumInputLength limit= 
-        Console.SetIn(new System.IO.StreamReader(Console.OpenStandardInput(limit), Console.InputEncoding, false, limit))
+type LineIo = { 
+                writer: string->unit
+                reader: unit ->string
+                prompt: unit->unit
+              }
 
-    let readLine() = Console.ReadLine().Trim()
+type Terminal(io: LineIo)=
+    let writeLine= io.writer
+    let prompt= io.prompt
 
-    let indexOfUserChoice labels = 
+    member this.readLine= io.reader
+
+    member this.indexOfUserChoice labels = 
         let line index text = sprintf "%02i-->%s" index text
         let menu = labels |> List.mapi line
-        menu |> List.iter (printfn "%s")
-        printfn "Enter the number of your choice or empty to return:"
-        printf ">"
-        let input = Console.ReadLine().Trim().ToLowerInvariant()
+        menu |> List.iter writeLine
+        writeLine "Enter the number of your choice or empty to return:"
+        prompt()
+        let input = this.readLine().ToLowerInvariant()
         parseInt input
 
-    let userChoice optionFormatter options =
+    member this.userChoice optionFormatter options =
         let optionForIndex index = List.item index options
-        options |> List.map optionFormatter |> indexOfUserChoice |> Option.map optionForIndex
+        options |> List.map optionFormatter |> this.indexOfUserChoice |> Option.map optionForIndex
    
 
-    let userEntry title= 
-        printfn "Please enter %s (empty returns):" title
-        printf ">"
-        match Console.ReadLine().Trim() with
+    member this.userEntry title= 
+        sprintf "Please enter %s (empty returns):" title |> writeLine
+        prompt()
+        match this.readLine() with
         | "" -> None
         | u -> Some(u)
 
 
-    let userChoiceOrFreeform title options =
-        let choice = "Enter manually" :: options |> indexOfUserChoice
+    member this.userChoiceOrFreeform title options =
+        let choice = "Enter manually" :: options |> this.indexOfUserChoice
         match choice with
-        | Some 0 -> userEntry title 
+        | Some 0 -> this.userEntry title 
         | Some index -> Some(List.item (index-1) options)
         | None -> None
 
-    let yesOrNo question=
-        printfn "%s (yes/no)?" question
+    member this.yesOrNo question=
+        sprintf "%s (yes/no)?" question |> writeLine
         let rec loop() = 
-            match Console.ReadLine().Trim().ToLowerInvariant() with 
+            match this.readLine().ToLowerInvariant() with 
             | "y"
             | "yes" -> true
             | "n"
             | "no" -> false
             | _ -> loop()
-        loop()
+        loop()    
+
+
+module Interaction=
+    let setupMaximumInputLength limit= 
+        Console.SetIn(new System.IO.StreamReader(Console.OpenStandardInput(limit), Console.InputEncoding, false, limit))
+
+    
+    let private terminal= new Terminal({
+                                            writer= printfn "%s"
+                                            reader= (fun () -> Console.ReadLine().Trim())
+                                            prompt= (fun() -> printf ">")
+                                      })
+    
+    let readLine = terminal.readLine
+    let indexOfUserChoice= terminal.indexOfUserChoice
+    let userChoice = terminal.userChoice
+    let userEntry = terminal.userEntry
+    let userChoiceOrFreeform= terminal.userChoiceOrFreeform
+    let yesOrNo= terminal.yesOrNo
+
+    
